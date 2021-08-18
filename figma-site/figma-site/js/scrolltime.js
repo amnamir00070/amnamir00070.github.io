cabd27e18e61659ec7048a04328499207959e1a9
Amna Mir <73982408+amnamir00070@users.noreply.github.com>
2021-08-18 08:48:13 -0700

    changed hover style

---
+++ figma-site/figma-site/js/scrolltime.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    // Add smooth scrolling to all links
+    $("a").on('click', function(event) {
+  
+      if (this.hash !== "") {
+
+        event.preventDefault();
+  
+        var hash = this.hash;
+  
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 1000, function(){
+     
+          window.location.hash = hash;
+        });
+      }
+    });
+  });
---
+++ figma-site/figma-site/js/search-popup.js
@@ -0,0 +1,10 @@
+function openSearch() {
+    document.getElementById("myOverlay").style.display = "block";
+  }
+  
+  function closeSearch() {
+    document.getElementById("myOverlay").style.display = "none";
+  }
+
+
+  
