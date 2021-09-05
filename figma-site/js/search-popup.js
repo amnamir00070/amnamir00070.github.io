d4c4773c5a6bc1700a52f9bcb86ecd08fcc2947f
amnamir00070 <amnamir00070@gmail.com>
2021-09-06 01:01:42 +0500

    made changes with button and search page bar

--- figma-site/js/search-popup.js
+++ figma-site/js/search-popup.js
@@ -1,9 +1,11 @@
 function openSearch() {
     document.getElementById("myOverlay").style.display = "block";
+    document.getElementById("bgFade").style.display = "none";    
   }
   
   function closeSearch() {
     document.getElementById("myOverlay").style.display = "none";
+    document.getElementById("bgFade").style.display = "block";
   }
 
 
