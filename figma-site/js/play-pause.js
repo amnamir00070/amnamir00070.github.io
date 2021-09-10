c488c71f1de7c49cbdc4c0cc0b4517341f3d9633
amnamir00070 <amnamir00070@gmail.com>
2021-09-10 11:49:00 +0500

    Made changes with the play button
    added hover in desktop and on-click in mobile effects in details page.

--- figma-site/js/play-pause.js
+++ figma-site/js/play-pause.js
@@ -1,6 +1,6 @@
-$('.clip').parent().click(function () {
-  if($(this).children(".clip").get(0).paused){        $(this).children(".clip").get(0).play();   $(this).children(".playpause").fadeOut();
-    }else{       $(this).children(".clip").get(0).pause();
+$('.mob-clip').parent().click(function () {
+  if($(this).children(".mob-clip").get(0).paused){        $(this).children(".mob-clip").get(0).play();   $(this).children(".playpause").fadeOut();
+    }else{       $(this).children(".mob-clip").get(0).pause();
   $(this).children(".playpause").fadeIn();
     }
 });
