9a58defbcead03d23d3ce5a4484cbb675ebb5563
amnamir00070 <amnamir00070@gmail.com>
2021-09-17 02:44:25 +0500

    Made alot of changes

--- figma-site/js/search-popup.js
+++ figma-site/js/search-popup.js
@@ -8,5 +8,19 @@ function openSearch() {
     document.getElementById("bgFade").style.display = "block";
   }
 
+// button
+var btn = $('#button');
 
-  
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
+
