cec01e672ac7bae72cb30801db6c3390b47a3819
amnamir00070 <amnamir00070@gmail.com>
2022-03-13 14:36:17 +0500

    jaan chor

--- zavirite/js/main.js
+++ zavirite/js/main.js
@@ -23,23 +23,44 @@ function openTab1() {
 	document.getElementById("tab1").style.display = "block";
 	document.getElementById("tab2").style.display = "none"; 
 	document.getElementById("tab3").style.display = "none";
-	document.getElementById("tab4").style.display = "none";     
+	document.getElementById("tab4").style.display = "none";
+	document.getElementById("tab5").style.display = "none";     
 }
 function openTab2() {
 	document.getElementById("tab1").style.display = "none";
 	document.getElementById("tab2").style.display = "block";
 	document.getElementById("tab3").style.display = "none"; 
 	document.getElementById("tab4").style.display = "none";     
+	document.getElementById("tab5").style.display = "none";
 }
 function openTab3() {
 	document.getElementById("tab1").style.display = "none";
 	document.getElementById("tab2").style.display = "none"; 
 	document.getElementById("tab3").style.display = "block"; 
-	document.getElementById("tab4").style.display = "none";    
+	document.getElementById("tab4").style.display = "none";
+	document.getElementById("tab5").style.display = "none";    
 }
 function openTab4() {
 	document.getElementById("tab1").style.display = "none";
 	document.getElementById("tab2").style.display = "none"; 
 	document.getElementById("tab3").style.display = "none"; 
-	document.getElementById("tab4").style.display = "block";    
+	document.getElementById("tab4").style.display = "block";
+	document.getElementById("tab5").style.display = "none";    
 }
+function openTab5() {
+	document.getElementById("tab1").style.display = "none";
+	document.getElementById("tab2").style.display = "none"; 
+	document.getElementById("tab3").style.display = "none"; 
+	document.getElementById("tab4").style.display = "none";
+	document.getElementById("tab5").style.display = "block";    
+}
+
+
+
+$(document).ready(function(){
+	$('.chart').horizBarChart({
+		selector: '.bar',
+		speed: 3000
+	});
+	});
+
